c1e7de0e4203c1a34098531d16ba963bc2212fd9
Rafael Costa <rafaelsevla@gmail.com>
2020-03-29 17:10:26 -0300

    remove trash code

--- src/screens/welcome/welcome.js
+++ src/screens/welcome/welcome.js
@@ -1,39 +1,14 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import t from 'prop-types'
 import { Text, View, Image, Alert } from 'react-native'
 import { Button } from 'react-native-elements'
 import { translate } from '~/locales'
 import MapView from 'react-native-maps'
 
-async function requestMapPermission() {
-  try {
-    const granted = await AndroidPermissions.requestPermission(
-      AndroidPermissions.PERMISSIONS.CAMERA,
-      {
-        title: 'Cool Photo App Camera Permission',
-        message:
-          'Cool Photo App needs access to your camera ' +
-          'so you can take awesome pictures.'
-      }
-    )
-    if (granted) {
-      console.log('You can use the camera')
-    } else {
-      console.log('Camera permission denied')
-    }
-  } catch (err) {
-    console.warn(err)
-  }
-}
-
 function Welcome(props) {
   const {
     navigation: { navigate }
   } = props
-
-  useEffect(() => {
-    requestMapPermission()
-  }, [])
 
   return (
     <View
